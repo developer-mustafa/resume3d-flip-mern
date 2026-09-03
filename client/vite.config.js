fdb13f1aed47cc1ecb3ce63efa85c36897801853
developer-mustafa <mustafarahmanctg@gmail.com>
2026-09-03 09:53:47 +0600

    fix: read version from root package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf-8'));
 
 export default defineConfig({
   plugins: [react()],
